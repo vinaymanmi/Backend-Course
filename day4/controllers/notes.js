0bf0a50941b071c157b74c82ed20eb1268412189
vinay manmi <vinaymanmi08@gmail.com>
2026-05-25 15:35:27 +0530

    apis

--- day4/controllers/notes.js
+++ day4/controllers/notes.js
@@ -24,4 +24,37 @@ const getNotes=async (req,res) => {
     }
 }
 
-module.exports={createNotebook,getNotes}
+const updateNotebook=async (req,res) => {
+    try {
+        const id=req.params.id;
+        const {name,heading,content}=req.body;
+        const updatenotebook=await notebook.findByIdAndUpdate(
+            id,{name,heading,content},{new:true}
+        )
+        if(!updatenotebook){
+            return res.send("Notes is not found ❌")
+        }
+        res.json({
+            message:"Notes updated✅",
+            updatenotebook
+        })
+    } catch (e) {
+        res.send(e.message);
+    }
+}
+
+const deleteNotebook= async (req,res) => {
+    try {
+        const id=req.params.id;
+        const notes=await notebook.findByIdAndDelete(id);
+        if(!notes){
+            return res.send("Notebook not found");
+        }
+        res.send("Notebook is deleted");
+    } catch (e) {
+        res.send(e.message)
+    }
+}
+
+
+module.exports={createNotebook,getNotes,updateNotebook,deleteNotebook}
--- day4/server.js
+++ day4/server.js
@@ -9,15 +9,19 @@ const port=process.env.port;
 const mongo_url=process.env.mongo_url;
 
 const {createAccount,login}=require("./controllers/user");
-const { createNotebook,getNotes} = require("./controllers/notes");
+const { createNotebook,getNotes,updateNotebook,deleteNotebook} = require("./controllers/notes");
 
 
 app.use(express.json());
 
 app.post("/signin",createAccount);
 app.post("/login",login);
-app.post("/notebook",createNotebook);
-app.get("/allNotes",getNotes);
+app.post("/notebook",createNotebook); //POST for send data
+app.get("/allNotes",getNotes);  //GET for getting/showing data
+//PUT for Updateing data
+app.put("/updatenotebook/:id",updateNotebook);
+
+app.delete("/api/deletenotebook/:id",deleteNotebook) //DELETE data
 
 mongoose.connect(mongo_url)
 .then(()=>{
