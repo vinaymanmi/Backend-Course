24a227cc638a42e01302914f992a7beec2952050
vinay manmi <vinaymanmi08@gmail.com>
2026-05-27 16:05:31 +0530

    signin

--- frontend/src/pages/Notebook.jsx
+++ frontend/src/pages/Notebook.jsx
@@ -1,8 +1,37 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios';
 
 const Notebook = () => {
+  const [notes,setNotes]=useState([]);
+  const getnotes=async ()=> {
+    const response= await axios.get("http://localhost:5000/allNotes");
+    // console.log(response.data.allNotebooks); 
+    setNotes(response.data.allNotebooks);
+  }
+
+
+  useEffect((getnotes)=>{},[])  
+
   return (
-    <div>Notebook page</div>
+    <div>
+      <h1>Availble notes...</h1> 
+
+      {notes.map((element, index, array) => {
+        // console.log(element.heading);
+        // console.log(element.content);
+      return (
+        <>
+          <h3>{element.heading}</h3>
+          <p>{element.content}</p>
+          <button>Update</button>
+          <button>Delete</button>
+          <hr>
+          </hr>
+        </>
+      )
+          
+      })}
+    </div> 
   )
 }
 
