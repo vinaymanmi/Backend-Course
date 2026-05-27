964db1bc85eb0242bd6557ec49f13e7d2c087c85
vinay manmi <vinaymanmi08@gmail.com>
2026-05-27 14:36:25 +0530

    signin

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,8 +1,42 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import axios from 'axios'
+import { useState } from 'react'
 
 const Login = () => {
+    const [email,setEmail] = useState("");
+    const [password,setPassword] = useState("")
+
+    const login=async () => {
+        const response = await axios.post("http://localhost:5000/login",{email,password});
+        alert(response.data.message)
+    }
   return (
-    <div>Login page</div>
+    <div>
+        <h2>Login Account</h2>
+
+        <input type="email" placeholder='Enter email' onChange={(e) => {
+            setEmail(e.target.value);
+        }}/>
+
+        <br /><br />
+
+        <input type="text" placeholder='Enter password' onChange={(e) => {
+            setPassword(e.target.value);
+        }}/>
+
+        {/* <h2>{email,password}</h2> */}
+        <br /><br />
+
+        <button onClick={login}>Login</button>
+
+        <br /><br />
+
+        <p>Don't have an account?
+            <Link to="/signin">   Sign in</Link>
+        </p>
+        
+    </div>
   )
 }
 
--- frontend/src/pages/Signin.jsx
+++ frontend/src/pages/Signin.jsx
@@ -1,8 +1,40 @@
 import React from 'react'
+import { useState } from 'react'
+import axios from 'axios'
+
+import { Link } from 'react-router-dom'
 
 const Signin = () => {
+    const [name,setName]=useState("")
+    const [email,setEmail]=useState("")
+    const [password,setPassword]=useState("")
+
+    const signin = async () =>{
+        const response = await axios.post("http://localhost:5000/signin",{name,email,password})
+        alert(response.data.message)
+    }
+
   return (
-    <div>Signin page</div>
+    <div>
+        <input type="text" placeholder='Enter Username' onChange={(e) => {
+            setName(e.target.value)
+        }}/>
+        <br /><br />
+        <input type="email" placeholder='Enter Email' onChange={(e) => {
+            setEmail(e.target.value)
+        }}/>
+        <br /><br />
+        <input type="password" placeholder='Enter password' onChange={(e) => {
+            setPassword(e.target.value)
+        }}/>
+
+        <br /><br />
+        <button onClick={signin}>Sign In</button>
+        <br /><br />
+        <p>Already have an account
+            <Link to="/">login</Link>
+        </p>
+    </div>
   )
 }
 
